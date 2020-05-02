5192ea771a6b4c93fd79b77e1a9b7ded7d9068a5
charleseduardome <charleseduardo.me@gmail.com>
2020-05-02 12:05:36 -0300

    Routes for search and delete

--- backend/src/routes/tools.routes.ts
+++ backend/src/routes/tools.routes.ts
@@ -1,14 +1,27 @@
 import { Router } from 'express';
-import { getRepository, TreeParent, Table } from 'typeorm';
+import { getRepository, Like } from 'typeorm';
 
 import CreateToolsService from '../services/CreateToolsService';
+import DeleteToolsService from '../services/DeleteToolsService';
 import Tools from '../models/Tools';
 
 const toolsRouter = Router();
 
 toolsRouter.get('/', async (request, response) => {
+  const { tag } = request.query;
+
   const toolsRepository = getRepository(Tools);
 
+  if (tag) {
+    const tools = await toolsRepository.find({
+      select: ['id', 'title', 'link', 'description', 'tags'],
+      where: {
+        tags: Like(`%${tag}%`),
+      },
+    });
+
+    return response.json(tools);
+  }
   const tools = await toolsRepository.find({
     select: ['id', 'title', 'link', 'description', 'tags'],
   });
@@ -21,19 +34,26 @@ toolsRouter.post('/', async (request, response) => {
 
   const createTools = new CreateToolsService();
 
-  await createTools.execute({
+  const tools = await createTools.execute({
     title,
     link,
     description,
     tags,
   });
 
-  return response.json({
-    title,
-    link,
-    description,
-    tags: tags.split(',').map(tag => tag.trim()),
-  });
+  delete tools.created_at;
+  delete tools.updated_at;
+
+  return response.status(201).json(tools);
+});
+
+toolsRouter.delete('/:id', async (request, response) => {
+  const { id } = request.params;
+  const deleteTools = new DeleteToolsService();
+
+  await deleteTools.execute({ id });
+
+  return response.status(204).send();
 });
 
 export default toolsRouter;
