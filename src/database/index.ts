a2e27ef3af69dc110fdc9bcfd13721c2f5b9bb75
charleseduardome <charleseduardo.me@gmail.com>
2020-05-04 20:10:34 -0300

    Config connection Heroku

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,3 +1,20 @@
 import { createConnection } from 'typeorm';
 
-createConnection();
+import * as PostgressConnectionStringParser from 'pg-connection-string';
+
+const connectionOptions = PostgressConnectionStringParser.parse(
+  process.env.DATABASE_URL,
+);
+
+return createConnection(<ConnectionOptions>{
+  driver: {
+    type: process.env.TYPEORM_DRIVER_TYPE,
+    host: connectionOptions.host,
+    port: connectionOptions.port || 5432,
+    username: connectionOptions.user,
+    password: connectionOptions.password,
+    database: connectionOptions.database,
+  },
+  entities: [...this.entities],
+  subscribers: [...this.subscribers],
+}).catch(error => console.log(error));
