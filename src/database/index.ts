73b1e92086ce70455202b14110356e19a6c6f983
charleseduardome <charleseduardo.me@gmail.com>
2020-05-04 20:50:46 -0300

    Config connection Heroku

--- src/database/index.ts
+++ src/database/index.ts
@@ -3,7 +3,6 @@ import { createConnection } from 'typeorm';
 createConnection({
   url: process.env.DATABASE_URL,
   type: 'postgres',
-  entities: ['./src/models/*.ts'],
   synchronize: true,
   ssl: true,
 });
