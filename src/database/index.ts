cf625acfa3472d724d7a865cf06f9237ad50690e
charleseduardome <charleseduardo.me@gmail.com>
2020-05-05 19:10:09 -0300

    Config connection Heroku

--- src/database/index.ts
+++ src/database/index.ts
@@ -9,7 +9,15 @@ if (process.env.NODE_ENV === 'production') {
     url: process.env.DATABASE_URL,
     type: 'postgres',
     entities: ['dist/models/*.js'],
+    migrations: ['dist/database/migrations/*.js'],
+    cli: {
+      entitiesDir: 'dist/entities',
+      migrationsDir: 'dist/database/migrations',
+      subscribersDir: 'dist/database/subscribers',
+    },
+    migrationsRun: true,
     synchronize: true,
+    logging: true,
     extra: {
       ssl: true,
     },
