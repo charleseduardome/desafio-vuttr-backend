8a43aace0016bfca65d0d578abc4b09147c8597f
charleseduardome <charleseduardo.me@gmail.com>
2020-05-05 17:51:27 -0300

    Config connection Heroku

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,26 +1,3 @@
 import { createConnection } from 'typeorm';
 
-createConnection({
-  type: 'postgres',
-  entities: ['./src/models/*.ts'],
-  migrations: ['./src/migrations/*.ts'],
-  migrationsRun: true,
-  synchronize: true,
-  url: process.env.DATABASE_URL,
-  ssl: true,
-});
-
-// export default async function buildConnectionPromise(): Promise<Connection> {
-//   const connection = await createConnection({
-//     type: 'postgres',
-//     entities: ['./src/models/*.ts'],
-//     migrations: ['./src/migrations/*.ts'],
-//     migrationsRun: false,
-//     synchronize: false,
-//     url: process.env.DATABASE_URL,
-//     ssl: true,
-//   });
-//   await connection.runMigrations();
-//   await connection.synchronize();
-//   return connection;
-// }
+createConnection();
