0b416eae996907a8d6ef8a7ff3e17c23069970ab
charleseduardome <charleseduardo.me@gmail.com>
2020-05-11 21:13:44 -0300

    Conexão cors

--- src/server.ts
+++ src/server.ts
@@ -13,9 +13,18 @@ import './database';
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(routes);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-app.use(cors());
+
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept',
+  );
+  next();
+});
 
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
   if (err instanceof AppError) {
