807e7cbb1f39681009effa897d0d0995396efb07
charleseduardome <charleseduardo.me@gmail.com>
2020-06-04 10:50:13 -0300

    Test of port for frontend

--- src/server.ts
+++ src/server.ts
@@ -40,6 +40,6 @@ app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
   });
 });
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log('(^o^) Server started on port 3000');
+app.listen(process.env.PORT || 3333, () => {
+  console.log('(^o^) Server started on port 3333');
 });
